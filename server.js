8f29ee20e01ee63ab8f67ff303c288ae9a138082
Noe Leiva <leiva.noe.86@gmail.com>
2020-08-15 23:53:43 -0700

    added folder structure to project. In addition, started main layout template

---
+++ server.js
@@ -0,0 +1,8 @@
+// Dependencies
+var express = require("express");
+// var mongojs = require("mongojs");
+var mongoose = require("mongoose");
+var ehbs = require("express-handlebars");
+// Require axios and cheerio. This makes the scraping possible
+var axios = require("axios");
+var cheerio = require("cheerio");
